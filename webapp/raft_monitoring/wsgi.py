98473c554e75c7e0ddf7f09e36c2b08e7f6646ee
Andrew <wertuk2@gmail.com>
2018-06-10 08:54:43 +0300

    Init commit

---
+++ webapp/raft_monitoring/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for raft_monitoring project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raft_monitoring.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
